e70d494798f658679b1e3b3a58dee5ff4e6dedd6
“Ajay <ajay.ip3105@gmail.com>
2022-08-14 21:40:22 -0400

    Accomplish task

---
+++ src/components/Navigation.js
@@ -0,0 +1,26 @@
+import React from "react";
+//import { NavLink } from "react-router-dom";
+import { NavLink } from "react-dom";
+const Navigation = () => {
+  return (
+    <div class="p-16">
+      <NavLink to="/" exact>
+        Home
+      </NavLink>
+      <br />
+      <NavLink to="/teams" exact>
+        Teams
+      </NavLink>
+      <br />
+      <NavLink to="/players/new" exact>
+        Create Player
+      </NavLink>
+      <br />
+      <NavLink to="/teams/new" exact>
+        Create Team
+      </NavLink>
+    </div>
+  );
+};
+
+export default Navigation;
